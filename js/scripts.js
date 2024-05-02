f28c73d4a6da270f4f23cbe43f90bac912ef993f
Nico <nicokrkcentral@gmail.com>
2024-05-02 18:59:21 -0300

    Segunda pre-entrega

--- js/scripts.js
+++ js/scripts.js
@@ -1,111 +1,216 @@
+// Función constructora
+class Propiedad {
+    constructor (name, tipo, ubicacion, precio, ambientes, tamañoTerreno, tamañoCubierto, cochera, baños, antiguedad, dormitorios){
+    this.name = name;
+    this.tipo = tipo;
+    this.ubicacion = ubicacion;
+    this.precio = precio;
+    this.ambientes = ambientes;
+    this.tamañoTerreno = tamañoTerreno;
+    this.tamañoCubierto = tamañoCubierto;
+    this.cochera = cochera;
+    this.baños = baños;
+    this.antiguedad = antiguedad;
+    this.dormitorios = dormitorios;
+}
+}
+let propiedades = [];
+
+//Funcion para agregar propiedad
+function agregarPropiedad (){
+    let name = prompt("Ingrese el nombre de propiedad")
+    let tipo = Number(prompt("Ingrese el tipo de propiedad: 1 Propiedad / 2 Terreno / 3 Galpon"))
+    let ubicacion = prompt ("Ingrese la ubicacion de la propiedad (ciudad)")
+    let precio = Number (prompt ("Ingrese el precio de la propiedad en peso argentino"))
+    let ambientes = Number(prompt("Ingrese cantidad de ambientes"))
+    let tamañoTerreno = Number(prompt("Ingrese metros cuadrados del terreno"))
+    let tamañoCubierto = Number(prompt("Ingrese metros cubiertos dentro del terreno"))
+    let cochera = prompt("Ingrese si tiene cochera (si o no)")
+    let baños = Number(prompt("Ingrese cantidad de baños en la propiedad"))
+    let antiguedad = Number(prompt("Ingrese cantidad de años de la propiedad"))
+    let dormitorios = Number(prompt("Ingrese cantidad de dormitorios que tiene la propiedad"))
+
+    let nuevaPropiedad = new Propiedad(name, tipo, ubicacion, precio, ambientes, tamañoTerreno, tamañoCubierto, cochera, baños, antiguedad, dormitorios);
+    propiedades.push(nuevaPropiedad);
+
+    alert ("La propiedad ha sido agregada con exito!")
+}
+
+//Funcion para filtrar propiedades
+function filtrarPropiedades(tipoPropiedad = null) {
+    console.log("Filtrando propiedades...");
+
+    if (propiedades.length === 0) {
+        console.log("No hay propiedades para filtrar.");
+        return;
+    }
+
+    let filtroUbicacion = prompt("Ingrese la ciudad donde desea comprar (deje en blanco si no es relevante)").toLowerCase();
+    let filtroAmbientes = prompt("Ingrese la cantidad de ambientes deseados (deje en blanco si no es relevante)");
+    let filtroDormitorios = prompt("Ingrese la cantidad de dormitorios deseados (deje en blanco si no es relevante)");
+    let filtroBaños = prompt("Ingrese la cantidad de baños deseados (deje en blanco si no es relevante)");
+    let filtroCochera = prompt("Desea con cochera: si / no (deje en blanco si no es relevante)").toLowerCase();
+
+    propiedades.forEach(propiedad => {
+        if ((filtroUbicacion === "" || propiedad.ubicacion.toLowerCase() === filtroUbicacion) &&
+            (filtroAmbientes === "" || propiedad.ambientes == filtroAmbientes || isNaN(filtroAmbientes)) &&
+            (filtroDormitorios === "" || propiedad.dormitorios == filtroDormitorios || isNaN(filtroDormitorios)) &&
+            (filtroBaños === "" || propiedad.baños == filtroBaños || isNaN(filtroBaños)) &&
+            (filtroCochera === "" || propiedad.cochera.toLowerCase() === filtroCochera) &&
+            (tipoPropiedad === null || propiedad.tipo === tipoPropiedad)) {
+            console.log(`Listado de propiedades filtradas: Nombre: ${propiedad.name}, Ambientes: ${propiedad.ambientes}, Ubicacion: ${propiedad.ubicacion}, Dormitorios: ${propiedad.dormitorios}, Baños: ${propiedad.baños}, Cochera: ${propiedad.cochera}, Tipo de Propiedad: ${propiedad.tipo}`);
+        }
+    });
+}
+
+// Función para mostrar terrenos 
+function mostrarTerrenos() {
+    console.log("Mostrando terrenos...");
+
+    if (propiedades.length === 0) {
+        console.log("No hay propiedades para mostrar.");
+        return;
+    }
+
+    propiedades.forEach(propiedad => {
+        if (propiedad.tipo === 2) {
+            console.log(`Nombre: ${propiedad.name}, Tamaño Terreno: ${propiedad.tamañoTerreno}m², Ubicación: ${propiedad.ubicacion}, Precio: $${propiedad.precio}`);
+        }
+    });
+}
+
+// Función para mostrar galpones 
+function mostrarGalpones() {
+    console.log("Mostrando galpones...");
+
+    if (propiedades.length === 0) {
+        console.log("No hay propiedades para mostrar.");
+        return;
+    }
+
+    propiedades.forEach(propiedad => {
+        if (propiedad.tipo === 3) { 
+            console.log(`Nombre: ${propiedad.name}, Ubicacion: ${propiedad.ubicacion}, Tamaño Terreno: ${propiedad.tamañoTerreno}m², Tamaño Cubierto: ${propiedad.tamañoCubierto}m², Antiguedad: ${propiedad.antiguedad}, Precio: $ ${propiedad.precio}.`);
+        }
+    });
+}
+
+
+
+// Función para mostrar todas las propiedades
+function mostrarPropiedades() {
+    document.write("Listado de propiedades");
+    propiedades.forEach((propiedad, index) => {
+        document.write("Propiedad " + (index + 1) + " Nombre: " + propiedad.name + " Tipo: " + propiedad.tipo + ", Ubicación: " + propiedad.ubicacion + ", Precio: " + propiedad.precio + ", Ambientes: " + propiedad.ambientes + ", Tamaño del terreno: " + propiedad.tamañoTerreno + ", Tamaño cubierto: " + propiedad.tamañoCubierto + ", Cochera: " + propiedad.cochera + ", Baños: " + propiedad.baños + ", Antigüedad: " + propiedad.antiguedad + ", Dormitorios: " + propiedad.dormitorios);
+    });
+    return;
+}
+
+
+
+
+//Funcion para contactar usuario
 function contactarUsuario (){
     alert ("Nos pondremos en contacto contigo a la brevedad");
 }
+//Funcion para mostrar error mensaje
 function error (){
-    alert ("Numero equivocado, por favor seleccione 1 o 2")
+    alert ("Numero equivocado, por favor seleccione 1, 2 o 3")
 }
-let name = prompt ("Bienvenido a JL INMOVILIARIA! Por favor ingrese su nombre");
-
-let phone;
-let phone1;
-do{
-    phone = prompt ("Por favor ingrese su telefono celular para estar en contacto");
-    phone1 = prompt("El numero del tel ingresado es " + phone + ". Es correcto?. si/no");
-    if (phone1 !== "si") {
-        alert ("vuelva a ingresar su numero telefonico");
-    }
-}while (phone1 !== "si");
-console.log ("Su nombre es " + name );
-console.log ("Su tel es " + phone);
-
-let info1;
-do {
-    info1 = Number (prompt("Perfecto. ¿Que desea hacer? Ingrese un numero: 1 Alquilar una propiedad / 2 Comprar una propiedad / terreno"));
-    if (info1 < 1 || info1 > 2){
-        error ();
-    }
- } while (info1 < 1 || info1 > 2);
-    console.log ((info1 === 1? "Alquiler" : "Compra"));
-
+const propiedad1 = 
+    new Propiedad('Propiedad 1', 1, 'Rosario', 250000, 4, 400, 200, 'si', 2, 10, 3);
+const propiedad2 = 
+    new Propiedad('Propiedad 2', 1, 'Rosario', 180000, 3, 300, 150, 'no', 1, 5, 2);
+const propiedad3 = 
+    new Propiedad('Propiedad 3', 1, 'Roldan', 25000, 0, 500, 0, 'no', 0, 0, 0);
+const propiedad4 = 
+    new Propiedad('Propiedad 4', 3, 'Roldan', 50000, 0, 800, 600, 'no', 0, 10, 0);
+const propiedad5 = 
+    new Propiedad('Propiedad 5', 3, 'Rosario', 60000, 0, 900, 500, 'no', 0, 10, 0);
+const propiedad6 = 
+    new Propiedad('Propiedad 6', 2, 'Roldan', 20000, 0, 400, 0, 'no', 0, 0, 0);
+const propiedad7 = 
+    new Propiedad('Propiedad 7', 3, 'Rosario', 350000, 4, 400, 200, 'si', 3, 2, 4);
+propiedades.push(propiedad1);
+propiedades.push(propiedad2);
+propiedades.push(propiedad3);
+propiedades.push(propiedad4);
+propiedades.push(propiedad5);
+propiedades.push(propiedad6);
+propiedades.push(propiedad7);
 
 
-    switch (info1) {
-        case 2:
+
+// Solicitar nombre o usuario JLinmobiliaria
+let usuario = prompt("Bienvenido a JL INMOBILIARIA! Por favor ingrese su nombre");
+
+if (usuario !== "JLinmobiliaria") {
+    let phone;
+    let phone1;
+    do {
+        phone = prompt("Por favor ingrese su telefono celular para estar en contacto");
+        phone1 = prompt("El numero del tel ingresado es " + phone + ". Es correcto?. si/no");
+        if (phone1 !== "si") {
+            alert("Vuelva a ingresar su numero telefonico");
+        }
+    } while (phone1 !== "si");
+    console.log("Su nombre es " + usuario);
+    console.log("Su tel es " + phone);
+   
+     let   info1 = Number(prompt("Perfecto. ¿Qué desea hacer? Ingrese un numero: 1 Ver todas las propiedades disponibles / 2 Filtrar propiedades"));
+        if (info1 < 1 || info1 > 2) {
+            error();
+        }
+        if (info1 === 1) {
+            mostrarPropiedades();
+            
+        }
+        if (info1 === 2) {
             let compra;
             do {
-                compra = Number(prompt("¿Qué desea comprar? 1 Propiedad / 2 Terreno"));
-                if (compra !== 1 && compra !== 2) {
+                compra = Number(prompt("¿Qué desea comprar? 1 Propiedad / 2 Terreno / 3 Galpon"));
+                if (compra < 1 || compra > 3) {
                     error();
-                } else if (compra === 1) {
-                    console.log("Propiedad");
-                    let room;
-                    do {
-                        room = Number(prompt("Ingrese cantidad de habitaciones"));
-                        if (room >= 1) {
-                            console.log(room + " Habitaciones");
-                        } else {
-                            alert ("Por favor, ingrese una cantidad válida de habitaciones.");
-                        }
-                    } while (room < 1);
+                }
+                if (compra === 1) {
+                    filtrarPropiedades (1);
                     
                 } else if (compra === 2) {
-                    console.log("Terreno");
+                    mostrarTerrenos();
+                    
+                } else if (compra === 3) {
+                    mostrarGalpones();
                     
                 }
-            } while (compra !== 1 && compra !== 2);
-        let province;
-            do{
-                province = Number (prompt (" Seleccione la provincia: 1 Santa Fe / 2 Cordoba"));
-                if (province < 1 || province > 2) {
-                    error ();
-            }
-        }while (province < 1 || province > 2);
-        console.log ("Provincia seleccionada :" + (province === 1 ? "Santa Fe" : "Cordoba" ));
-        contactarUsuario();
-            break;
-        
-
-    case 1:
-        let province2;
-        do{
-            province2 = Number (prompt (" Seleccione la provincia: 1 Santa Fe / 2 Cordoba"));
-            if (province2 < 1 || province2 > 2) {
-                error ();
+            } while (compra !== 1 && compra !== 2 && compra !== 3);
         }
-    }while (province2 < 1 || province2 > 2);
-    console.log ("Provincia de " + (province2 === 1 ? "Santa Fe" : "Cordoba" ));
-
-    let tipo
-    do{
-        tipo = Number (prompt ("Ingrese una opcion: 1 Departamento / 2 Casa / 3 Galpon"));
-        if (tipo < 1 || tipo > 3){
-           error ();
-        }else if (tipo === 3){
-            console.log ("Galpon");
-            contactarUsuario ();
-            
-            break;
-        }
-     } while (tipo < 1 || tipo > 3);
-     
     
-    if (tipo !== 3) { 
-        console.log((tipo === 1 ? "Departamento" : "Casa"));
-    let room2;
+} else {
+    let clave;
     do {
-        room2 = Number (prompt("Ingrese cantidad de habitaciones"));
-        if (room2 === 1 || room2 === 2 ){
-            console.log (room2 + " Habitaciones")
-        }            
-        else if (room2 > 2) {
-            console.log (room2 + " Habitaciones")
-        }   
-    }while (room2 < 1);
-    
-    contactarUsuario ();
-}
-    break;
+        clave = Number(prompt("Bienvenido! Ingrese la clave"));
+        if (clave !== 147258) {
+            alert("Clave incorrecta, vuelva a ingresarla");
+        }
+    } while (clave !== 147258);
 
-    default :
-    error ();
+    let tarea;
+    do {
+        tarea = Number(prompt("Que desea hacer? 1 Cargar una propiedad / 2 Mostrar todas las propiedades / 3 Salir/ 4 Filtrar propiedades"));
+        if (tarea === 3) {
+            alert("Hasta luego, nos vemos pronto");
+            break;
+        } else if (tarea === 1) {
+            agregarPropiedad();
+        } else if (tarea === 4 ){
+            filtrarPropiedades ();
+            
+        } else if (tarea === 2) {
+            mostrarPropiedades();
+            
+        }
+    } while (tarea !== 3);
 }
+
+
+
